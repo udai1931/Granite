972acadca504dc1dd1dec490805edf85aa03f160
Udai Gupta <udaigupta19311@gmail.com>
2022-05-13 18:40:50 +0530

    Added migration script for creating tasks table

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books do
+    member do
+      get 'delete_multiple'
+    end
+  end
 end
