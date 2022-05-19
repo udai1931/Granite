9959fcb9c56abf4e72d481a5457b49eacae8fb6d
Udai Gupta <udaigupta19311@gmail.com>
2022-05-19 15:08:38 +0530

    Added has_secure_password and validations to User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # resources :books do
-  #   member do
-  #     get 'delete_multiple'
-  #   end
-  # end
-  constraints(lambda {|req| req.format =  :json}) do
+  constraints(lambda { |req| req.format == :json }) do
     resources :tasks, except: %i[new edit], param: :slug
     resources :users, only: :index
   end
 
   root "home#index"
-  get '*path', to: 'home#index', via: :all
+  get "*path", to: "home#index", via: :all
 end
