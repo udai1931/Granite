22e3e764b8657859fa8d96b0044eddea91e87dc8
udai1931 <udaigupta19311@gmail.com>
2022-05-25 19:49:21 +0530

    Added heroku sidekiq configs

--- app/models/task.rb
+++ app/models/task.rb
@@ -16,6 +16,7 @@ class Task < ApplicationRecord
 
   before_create :set_slug
   after_create :log_task_details
+  after_commit :log_task_details, on: :create
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
+require "sidekiq/web"
+require "sidekiq/cron/web"
+
 Rails.application.routes.draw do
+
+  def draw(routes_name)
+    instance_eval(File.read(Rails.root.join("config/routes/#{routes_name}.rb")))
+  end
+
+  draw :sidekiq
+
   constraints(lambda { |req| req.format == :json }) do
     resources :tasks, except: %i[new edit], param: :slug
     resources :users, only: %i[index create]
